55ee2bd742aa23063c4bcc5caaa8500b1ab0f37f
danisantosss <danisantos5075@gmail.com>
2022-09-27 13:40:43 -0300

    terceiro módulo

--- projetoweb/index.js
+++ projetoweb/index.js
@@ -25,7 +25,7 @@ const fakeData = [
     {
         id: 2,
         nome: 'Mariazinha',
-        endereco: 'Rua lululu 200',
+        endereco: 'Rua lulululu 200',
         sexo: 'Feminino',
         telefone: '5555-4321'
     }
@@ -37,6 +37,16 @@ app.get("/", function(req,res){
     res.render('index');
 });
 
+app.get("/clientes/delete/:id", function(req,res){
+    let umcliente = fakeData.find(o => o.id == req.params["id"]);
+    let posicao = fakeData.indexOf(umcliente);
+    if(posicao > -1){
+        fakeData.splice(posicao,1); //remove um elemento da lista na posicao informada
+    }
+    res.redirect("/clientes");
+});
+
+
 app.get("/clientes", function(req,res){
     res.render('cliente/cliente',{listaclientes: fakeData});
 });
@@ -45,31 +55,50 @@ app.get("/clientes/novo", function(req,res){
     res.render("cliente/formcliente");
 });
 
+app.get("/clientes/alterar/:id", function(req,res){
+    let id = req.params['id'];
+    let umcliente = fakeData.find(o => o.id == id);
+    
+    res.render('cliente/formcliente',{cliente: umcliente});
+});
+
 app.post("/clientes/save", function(req,res){
-    //console.log(req.body.nome);
+    //console.log(req.body);
     // Criando um novo objeto JS com o atributo nome
     //Math.max()
+    if(req.body.nome === ""){
+        //res.redirect("/clientes");
+        res.render('cliente/formcliente',{cliente: req.body});
+        return;
+    }
 
-    let maiorid = Math.max(...fakeData.map( o => o.id ));
-    if(maiorid == -Infinity) maiorid = 0;
-    /*
-        const fakedata = [ {id: 1, nome: 'ze}, {id:2, nome: 'lu'}];
-        fakeData.map( o => o.id ) Transforma em
-        const outralista = [ {id:1}, {id: 2}];
-        ...fakeData.map( o => o.id ) Transforma em
-        const listanumeros = [1,2];
-    */
-   
-   
-   
-    let novoCliente = {
-        id: maiorid + 1,
-        nome: req.body.nome,
-        endereco: req.body.endereco,
-        sexo: req.body.sexo,
-        telefone: req.body.telefone
-    };
-    fakeData.push(novoCliente);
+    let clienteantigo = fakeData.find( o => o.id == req.body.id);
+
+    if(clienteantigo != undefined){
+        clienteantigo.nome = req.body.nome;
+        clienteantigo.endereco = req.body.endereco;
+        clienteantigo.sexo = req.body.sexo;
+        clienteantigo.telefone = req.body.telefone;
+    }else{
+
+        let maiorid = Math.max(...fakeData.map( o => o.id ));
+        if (maiorid == -Infinity) maiorid = 0;
+        /*
+            const fakedata = [ {id: 1, nome: 'ze'}, {id: 2, nome: 'lu'}];
+            fakeData.map( o => o.id ) Transforma em
+            const outralista = [ {id: 1}, {id: 2}];
+            ...fakeData.map( o => o.id ) Transforma em
+            const listanumeros = [1,2];
+        */
+        let novoCliente = {
+            id: maiorid + 1,
+            nome: req.body.nome,
+            endereco: req.body.endereco,
+            sexo: req.body.sexo,
+            telefone: req.body.telefone
+        };
+        fakeData.push(novoCliente);
+    }
     res.redirect("/clientes");
 });
 
